957988aba6900fd9e2f0af968a931d03f0455fc0
William Wakefield <wakeforest.ww@gmail.com>
2019-03-03 00:59:56 -0500

    Inde/htmlx must be top-level

---
+++ components/contact/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                Contact Me!
+            </div>
+        );
+    }
+}
+
+export default Contact;
